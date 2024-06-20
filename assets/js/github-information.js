3ce428af416195fe210284cf151a8e4da095b62c
hysinh <156194500+hysinh@users.noreply.github.com>
2024-06-20 19:18:11 +0100

    Renders user information from github

---
+++ assets/js/github-information.js
@@ -0,0 +1,46 @@
+function userInformationHTML(user) { // user is the object that is returned from the GitHub API
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            
+            <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
+            </div>`;
+}
+
+function fetchGitHubInformation(event) {
+    var username = $("#gh-username").val();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    }
+
+    $("#gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) { // response = response we get back from our getJSON() method
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData)); // user jQuery selectos to select the gh-user-data div and set the HTML to the results of another function - userInformationHTML()
+        }, function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
+    
+}
